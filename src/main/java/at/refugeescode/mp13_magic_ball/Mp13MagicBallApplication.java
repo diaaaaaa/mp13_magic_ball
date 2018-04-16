5748d6929f320b7d63334629775b14800afb9822
diaaaaaa <ite.diaa@gmail.com>
2018-04-16 17:07:54 +0200

    spring boot + thymleaf

---
+++ src/main/java/at/refugeescode/mp13_magic_ball/Mp13MagicBallApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp13_magic_ball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp13MagicBallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp13MagicBallApplication.class, args);
+	}
+}
